4495c35af6c5155fd3d00e3a6877ac14b7e7fd7d
Andrii Mysnyk <andrii_mysnyk@epam.com>
2015-10-22 14:51:18 +0300

    distance calculation functionality moved from controller to separate service (distanceCalculatorService); controller renamed

---
+++ src/AngularMvc6/Scripts/distanceCalculatorService.js
@@ -0,0 +1,37 @@
+﻿/**
+ * @module distanceCalculatorService
+ * @see http://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+ * @overview Haversine formula implementation for distance calculation
+ */
+
+(function () {
+    'use strict';
+
+    function distanceCalculatorService() {
+
+        function deg2Rad(deg) {
+            return deg * (Math.PI / 180);
+        }
+
+        function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+            var r = 6371; // Radius of the earth in km
+            var dLat = deg2Rad(lat2 - lat1);  // deg2rad below
+            var dLon = deg2Rad(lon2 - lon1);
+            var a =
+              Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(deg2Rad(lat1)) * Math.cos(deg2Rad(lat2)) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            var d = r * c; // Distance in km
+            return d;
+        }
+        
+        return {
+            calculate: getDistanceFromLatLonInKm
+        };
+    }
+
+    angular
+        .module('app')
+        .factory('distanceCalculatorService', distanceCalculatorService);
+})();
--- src/AngularMvc6/Scripts/distanceController.js
+++ src/AngularMvc6/Scripts/distanceController.js
@@ -1,25 +1,8 @@
 ﻿(function () {
     'use strict';
 
-    function deg2Rad(deg) {
-        return deg * (Math.PI / 180);
-    }
-
-    function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-        var r = 6371; // Radius of the earth in km
-        var dLat = deg2Rad(lat2 - lat1);  // deg2rad below
-        var dLon = deg2Rad(lon2 - lon1);
-        var a =
-          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-          Math.cos(deg2Rad(lat1)) * Math.cos(deg2Rad(lat2)) *
-          Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = r * c; // Distance in km
-        return d;
-    }
-
-    function distancesController($scope, stopsResource) {
-        $scope.title = 'distancesController';
+    function distanceController($scope, stopsResource, distanceCalculatorService) {
+        $scope.title = 'distanceController';
 
         stopsResource.getAll().$promise.then(function (result) {
             $scope.stops = result;
@@ -31,7 +14,7 @@
                 return;
             }
 
-            var distance = getDistanceFromLatLonInKm(
+            var distance = distanceCalculatorService.calculate(
                 $scope.stop1.originalObject.Latitude,
                 $scope.stop1.originalObject.Longitude,
                 $scope.stop2.originalObject.Latitude,
@@ -52,7 +35,7 @@
 
     angular
         .module('app')
-        .controller('distancesController', distancesController);
+        .controller('distanceController', distanceController);
 
-    distancesController.$inject = ['$scope', 'stopsResource'];
+    distanceController.$inject = ['$scope', 'stopsResource', 'distanceCalculatorService'];
 })();
